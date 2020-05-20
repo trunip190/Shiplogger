540acc506efe3de28f336fbab2480925e7382c68
Chris Woollacott <chrisw@mcgregor-hardware.com>
2020-05-20 12:28:13 -0700

    Add project files.

---
+++ Shiplogger/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Shiplogger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnPick = new System.Windows.Forms.Button();
+            this.fbdFolder = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(376, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 19);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Location";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(455, 151);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(455, 68);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedValueChanged += new System.EventHandler(this.listBox1_SelectedValueChanged);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(10, 35);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Size = new System.Drawing.Size(455, 222);
+            this.splitContainer1.SplitterDistance = 68;
+            this.splitContainer1.SplitterWidth = 3;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(98, 12);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(275, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "C:\\Users\\chrisw\\purofiles";
+            // 
+            // btnPick
+            // 
+            this.btnPick.Location = new System.Drawing.Point(10, 11);
+            this.btnPick.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnPick.Name = "btnPick";
+            this.btnPick.Size = new System.Drawing.Size(83, 19);
+            this.btnPick.TabIndex = 5;
+            this.btnPick.Text = "Load Files";
+            this.btnPick.UseVisualStyleBackColor = true;
+            this.btnPick.Click += new System.EventHandler(this.btnPick_Click);
+            // 
+            // fbdFolder
+            // 
+            this.fbdFolder.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.fbdFolder.ShowNewFolderButton = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 273);
+            this.Controls.Add(this.btnPick);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MinimumSize = new System.Drawing.Size(492, 312);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnPick;
+        private System.Windows.Forms.FolderBrowserDialog fbdFolder;
+    }
+}
+
---
+++ Shiplogger/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Shiplogger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public List<string> PopulateFiles(string location)
+        {
+            string[] _files = Directory.GetFiles(location);
+
+            return _files.Where(o => o.ToLower().EndsWith("csv")).ToList();
+        }
+
+        public void LoadFile(string FileLocation)
+        {
+            String[] Lines = File.ReadAllLines(FileLocation);
+
+            foreach ( string s in Lines)
+            {
+                System.Diagnostics.Debug.WriteLine(s);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnPick_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(textBox1.Text))
+            {
+                if ( fbdFolder.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.Text = fbdFolder.SelectedPath;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            listBox1.BeginUpdate();
+            listBox1.Items.Clear();
+            List<string> Files = PopulateFiles(textBox1.Text);
+            System.Diagnostics.Debug.WriteLine($"{Files.Count} Files.");
+
+            foreach (string s in Files)
+            {
+                listBox1.Items.Add(s);
+            }
+            listBox1.EndUpdate();
+        }
+
+        private void listBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem.ToString() == "")
+                return;
+
+            string FileLocation = listBox1.SelectedItem.ToString();
+
+            listView1.BeginUpdate();
+            listView1.Clear();
+
+            string[] Lines = File.ReadAllLines(FileLocation);
+            string[] ColumnsToAdd = Lines[0].Split(',');
+            string[] EntryToAdd;
+
+            foreach (string s in ColumnsToAdd)
+            {
+                listView1.Columns.Add(s);
+            }
+
+            for (int i = 1; i < Lines.Length; i++) //start at 1 to skip column headers
+            {
+                EntryToAdd = Lines[i].Split(',');
+                ListViewItem lvi = new ListViewItem(EntryToAdd[0]);
+                lvi.SubItems.AddRange(EntryToAdd);
+
+                listView1.Items.Add(lvi);
+            }
+
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            listView1.EndUpdate();
+        }
+    }
+}
